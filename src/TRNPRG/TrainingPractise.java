e8c2024d29aa37437ca922438920f410f680c5b8
Siddhant Anand <SiddhantA@interrait.com>
2022-12-14 12:30:04 +0530

    commit form Architect

--- src/TRNPRG/TrainingPractise.java
+++ src/TRNPRG/TrainingPractise.java
@@ -5,6 +5,7 @@ public class TrainingPractise {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is content of master branch");
+System.out.println("This is content of Architect1 branch");
 	}
 
 }
